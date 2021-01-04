9e07420a7d270a7595be4a434b2444d31fa52f65
Emily Overend <eo17233@gmail.com>
2021-01-04 01:23:57 +0000

    Automatically backed up by Learn

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -7,12 +7,12 @@ def fizzbuzz(number)
     "Fizzbuzz"
   elsif number % 3 == 0
     "Fizz"
-  elsif number % 5 == 0 
+  elsif number % 5 == 0
     "Buzz"
-  else 
-    Number 
-  end 
-end 
+  else
+    Number
+  end
+end
 
 describe "fizzbuzz" do
   it 'returns "Fizz" when the number is divisible by 3' do
